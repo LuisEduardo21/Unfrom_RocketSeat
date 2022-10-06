109ec8bc5cc7fcb274148b66cb01bd045d321466
Luis Eduardo <lfdasilva9@gmail.com>
2022-10-05 22:48:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Form } from '@unform/web';
 import './App.css';
+import Input from './components/Form/input';
+
+const initialData = {
+  email: 'test@example.com',
+}
 
 function App() {
+  function handleSubmit(data) {
+    console.log(data);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Hello World</h1>
+
+       <Form initialData={initialData} onSubmit={handleSubmit}>
+         <Input name = "name"/>
+         <Input type = 'email' name = "email"/>
+         <Input type = 'password' name = "password"/>
+
+         <button type='submit'>Enviar</button>
+       </Form>
     </div>
   );
 }
