70598d671ef9f0d73673cef941b1cdc27b53445f
Luis Eduardo <lfdasilva9@gmail.com>
2022-10-06 12:33:16 -0300

    finish project

--- src/App.js
+++ src/App.js
@@ -1,35 +1,63 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef} from "react";
 import { Form } from "@unform/web";
 import { Scope } from "@unform/core";
 import "./App.css";
+import * as Yup from "yup";
 import Input from "./components/Form/input";
 
-const initialData = {
-  email: "test@example.com",
-  address: {
-    city: "San Pablo",
-  },
-};
 
 function App() {
   const formRef = useRef(null);
 
-  function handleSubmit(data) {
-    if (data.name === "") {
-      formRef.current.setErrors({
-       name: "o nome é obrigatorio",
-       address: {
-         city: "a cidade é obrigatorio",
-       }
-     })
+  async function handleSubmit(data, { reset }) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("o nome é obrigatório"),
+        email: Yup.string()
+          .email("Digite um email válido")
+          .required("o email é obrigatório"),
+        address: Yup.object().shape({
+          city: Yup.string()
+            .min(3, "No minimo 3 caracteres")
+            .required("a cidade é obrigatório"),
+        }),
+      });
+
+      await schema.validate({
+        abortEarly: false,
+      });
+      console.log(data);
+      formRef.current.setError({});
+      reset();
+      
+    }catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errorMessages = {};
+
+        err.inner.forEach(error => {
+          errorMessages[error.path] = error.message;
+        })
+
+        formRef.current.setError(errorMessages);
     }
-    console.log(data);
   }
+
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      formRef.current.setData({
+        name: 'luis teste',
+        email: 'teste@example.com',
+      })
+    }, 200);
+  }, [])
+
   return (
     <div className="App">
       <h1>Hello World</h1>
 
-      <Form initialData={initialData} onSubmit={handleSubmit}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <Input name="name" />
         <Input type="email" name="email" />
         <Input type="password" name="password" />
